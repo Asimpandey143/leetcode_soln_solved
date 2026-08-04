04780fa20c4bc007ec1128ac58a29c689f9fac0a
Asimpandey143 <pandeyasim27@gmail.com>
2026-08-04 23:53:40 +0530

    Time: 3 ms (49.33%), Space: 33.8 MB (8.08%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        unordered_set<int> present(nums.begin(), nums.end());
+        int minVal = *min_element(nums.begin(), nums.end());
+        int maxVal = *max_element(nums.begin(), nums.end());
+        
+        vector<int> ans;
+        for (int i = minVal; i <= maxVal; ++i) {
+            if (present.find(i) == present.end()) {
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
